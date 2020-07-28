d1bac9b18f9429a09709f5cf229793bdac739b5d
datns <nsdat1802@gmail.com>
2020-07-28 21:41:39 +0700

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+let messageOne = document.querySelector("#message-1");
+let messageTwo = document.querySelector("#message-2");
+
+weatherForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const location = search.value;
+	messageOne.textContent = "Loading...";
+	messageTwo.textContent = "";
+	fetch("http://localhost:3000/weather?address=" + location).then(response => {
+		response.json().then(data => {
+			if (data.error) {
+				console.log(data.error);
+				messageOne.textContent = data.error
+			} else {
+				console.log(data.forecast);
+				messageOne.textContent = location;
+				messageTwo.textContent = data.forecast;
+			}
+		})
+	})
+});
+
---
+++ src/app.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const path = require('path');
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+// Define paths for Express config
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "DDns"
+  })
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "DDns"
+  })
+});
+
+app.get("/help", ((req, res) => {
+  res.render("help", {
+    helpText: "This is a helpful text.",
+    title: "Help",
+    name: "DDns"
+  });
+}));
+
+app.get('/weather', (req, res) => {
+  const address = req.query.address;
+  if (!address) {
+      return res.send({
+          error: "You must provide an address"
+      });
+  }
+  geocode(address, (error, { latitude, longitude, location } = {}) => {
+    if (error)
+      return res.send({
+        error
+      });
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error)
+        return res.send({ error });
+      return res.send({
+        forecast: forecastData,
+        location,
+        address
+      })
+    })
+  });
+});
+
+app.get("/help/*", (req, res) => {
+    res.render("404", {
+        text: "Help article not found.",
+        title: "404",
+        name: "DDns"
+    })
+});
+
+app.get("*", (req, res) => {
+    res.render("404", {
+        text: "Page not found.",
+        title: "404",
+        name: "DDns"
+    })
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000.')
+});
+
