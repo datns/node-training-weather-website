6c50f9303a8cb2ce753de1383421c51ef5f8a8be
datns <nsdat1802@gmail.com>
2020-07-30 21:15:49 +0700

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 // Define paths for Express config
-const app = express()
+const app = express();
+const port = process.env.PORT || 3000;
+
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
@@ -80,7 +82,7 @@ app.get("*", (req, res) => {
     })
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is up on port 3000.')
 });
 
